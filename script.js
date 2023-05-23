4decec036cb5bed17dea833baaeb5071d3e7b75e
griffinnewbold <griffinnewbold@gmail.com>
2023-05-23 01:24:06 -0400

    finished project for now!

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let currentDiv = 1;
-const totalDivs = 4;
-
-
 let currentTeachingDiv = 1;
+let currentProjectDiv = 1;
+const totalProjectDivs = 9;
 const totalTeachingDivs = 2;
+const totalDivs = 4;
 
 function showNextDiv() {
     const currentDivId = `div${currentDiv}`;
@@ -72,7 +72,43 @@ function showPrevDivTeach() {
         currentTeachingDiv--;
     }else{
         currentDivElem.classList.remove('active');
-        currentTeachingDiv = 2;
+        currentTeachingDiv = totalTeachingDivs;
         document.getElementById('div2').classList.add('active');
+    }
+}
+
+function showNextDivProject(){
+    const currentDivId = `div${currentProjectDiv}`;
+    const nextDivId = `div${currentProjectDiv + 1}`;
+
+    const currentDivElem = document.getElementById(currentDivId);
+    const nextDivElem = document.getElementById(nextDivId);
+
+    if(nextDivElem){
+        currentDivElem.classList.remove('active');
+        nextDivElem.classList.add('active');
+        currentProjectDiv++;
+    }else{
+        currentDivElem.classList.remove('active');
+        currentProjectDiv = 1;
+        document.getElementById('div1').classList.add('active');
+    }
+}
+
+function showPrevDivProject(){
+    const currentDivId = `div${currentProjectDiv}`;
+    const nextDivId = `div${currentProjectDiv - 1}`;
+
+    const currentDivElem = document.getElementById(currentDivId);
+    const nextDivElem = document.getElementById(nextDivId);
+
+    if(nextDivElem){
+        currentDivElem.classList.remove('active');
+        nextDivElem.classList.add('active');
+        currentProjectDiv--;
+    }else{
+        currentDivElem.classList.remove('active');
+        currentProjectDiv = totalProjectDivs;
+        document.getElementById('div9').classList.add('active');
     }
 }
